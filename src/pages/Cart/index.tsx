dfe4f7d1f3543ce76e0e2accaef217eb7742667a
Anderson de Oliveira <amatos.oliveira2197@gmail.com>
2021-01-20 05:47:21 -0300

    update

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -40,22 +40,36 @@ const Cart: React.FC = () => {
 
   function handleIncrement(id: string): void {
     // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
     // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
 
-    return formatValue(0);
+    const total = products.reduce((accumulator, product) => {
+      const productsSubtotal = product.price * product.quantity;
+
+      return accumulator + productsSubtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
 
-    return 0;
+    const total = products.reduce((accumulator, product) => {
+      const productsQuantity = product.quantity;
+
+      return accumulator + productsQuantity;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
@@ -89,6 +103,7 @@ const Cart: React.FC = () => {
               </ProductTitleContainer>
               <ActionContainer>
                 <ActionButton
+
                   testID={`increment-${item.id}`}
                   onPress={() => handleIncrement(item.id)}
                 >
@@ -97,6 +112,7 @@ const Cart: React.FC = () => {
                 <ActionButton
                   testID={`decrement-${item.id}`}
                   onPress={() => handleDecrement(item.id)}
+
                 >
                   <FeatherIcon name="minus" color="#E83F5B" size={16} />
                 </ActionButton>
